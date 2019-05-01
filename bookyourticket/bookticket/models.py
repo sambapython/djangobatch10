0484cf78e652aa533d80b06ce7b449090c9850e4
sambapython@gmail.com <sambapython@gmail.com>
2019-05-01 10:31:18 +0530

    2019-05-01 model and shell CRUD operations.

--- bookyourticket/bookticket/models.py
+++ bookyourticket/bookticket/models.py
@@ -1,8 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Language(models.Model):
+	name=models.CharField(max_length=60)
+
+
 class Movie(models.Model):
+	#languages=[("t","TELUGU"),("e","ENGLISH"),("h","HINDHI")]
 	name = models.CharField(max_length=300)
+	description = models.TextField(blank=True, null=True)
+	cast = models.CharField(max_length=250,blank=True, null=True)
+	languages = models.ManyToManyField(Language)
+	twod = models.BooleanField(default=True)
+	threed = models.BooleanField(default=False)
+	rating = models.IntegerField(default=1)
+	createdby=models.ForeignKey(User, blank=True, null=True, on_delete=models.PROTECT)
 
-
+	
 
